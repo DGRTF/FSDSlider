887b1e7883150e14fbeb9069230b618f4d2308d5
Your Name <futunplay@gmail.com>
2020-09-22 15:19:08 +0300

    Add description

--- src/Model/AddLIstener.ts
+++ src/Model/AddLIstener.ts
@@ -1,5 +1,8 @@
 import { IModelObserver } from "./model";
 
+// Обертка над обработчиком события в виде класса
+// Далее эту обертку можно использовать для любого класса,
+// добавляющего слушателя с интерфейсом IModelObserver
 export default class AddListener implements IModelObserver {
   constructor(listener: (selectValue: string) => void) {
     this.listener = listener;
